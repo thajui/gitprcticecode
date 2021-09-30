acc1bde10a8d7c7e67b33b66cf397b760f29b502
thajui <thajwnp@gmail.com>
2021-09-30 11:16:12 +0530

    terraform files

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable client_id {
+  
+}
+
+variable client_secret {
+  
+}
+
+variable ssh_public_key {
+  
+}
+variable environment {
+    default = "dev"
+}
+
+variable location {
+  default = "eastus"
+}
+
+variable node_count {
+  default = 3
+}
+
+variable dns_prefix {
+    default = "k8test"
+  
+}
+
+variable resource_group {
+
+    default = "kubernets"
+  
+}
