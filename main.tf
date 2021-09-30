588a4bb82a522636a57b49c6f3ed528a14206f29
thajui <thajwnp@gmail.com>
2021-09-30 11:15:10 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "resource_group" {
+  name = "${var.resource_group}_${var.environment}"
+  location = var.location
+}
+resource "azurerm_kubernetes_cluster" "terraformk8s" {
+    name = "${var.clustername}_${var.environment}"
+    location = var.location
+    resource_group_name = azurerm_resource_group.resource_group.name
+    dns_prefix = var.dns_prefix
+
+    linux_profile {
+      admin_username = "ubuntu"
+    }
+    ssh_key {
+        key_data = file(var.ssh_public_key)
+    }
+}
+default_node_pool{
+ name = "agentpool"
+ node_count = var.node_count
+ vm_size = "Standard_B2s"
+
+}
+service_principal{
+    client_id = var.client_id
+    client_secret = var.client_secret
+    tags = {
+        environment = var.environment
+    }
+}
+
+terraform {
+  backend "azurerm" {
+
+  }
+  
+  }
