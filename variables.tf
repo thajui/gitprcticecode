e4145be7fa2d609001a20b6e705329d5fedc5ffa
thajui <thajwnp@gmail.com>
2021-10-01 10:09:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,29 @@
-variable client_id {
-  
-}
+variable client_id {}
+variable client_secret {}
+variable ssh_public_key {}
 
-variable client_secret {
-  
-}
-
-variable ssh_public_key {
-  
-}
 variable environment {
     default = "dev"
 }
 
 variable location {
-  default = "eastus"
+    default = "westeurope"
 }
 
 variable node_count {
-  default = 3
+  default = 2
 }
 
+
+
 variable dns_prefix {
-    default = "k8test"
-  
+  default = "k8stest"
+}
+
+variable cluster_name {
+  default = "k8stest"
 }
 
 variable resource_group {
-
-    default = "kubernets"
-  
+  default = "kubernetes"
 }
